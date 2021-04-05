b5bd91558d14058d6d8bb8eab93481d4741c2687
Ahmed Jundy <63857459+Ahmedjunidy3@users.noreply.github.com>
2021-04-05 21:51:44 +0200

    Add files via upload

---
+++ LoginFragment.java
@@ -0,0 +1,35 @@
+package com.example.qrxscanners.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.qrxscanners.R;
+
+
+public class LoginFragment extends Fragment {
+
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
